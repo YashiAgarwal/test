b9c75e0ba6877bbb0dc873976b9f052eed25e537
YashiAgarwal <agarwalyashi1992.ya@gmail.com>
2018-03-28 09:20:40 +0530

    check ye wali file

---
+++ src/main/StartProgram.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Yashi Agarwal on 28-03-2018.
+ */
+public class StartProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
